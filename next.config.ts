211a13f5d7370e16de0430157968c31065c5a21b
Ye Win Myat <yewinmyat@Yes-MacBook-Air-2.local>
2025-03-25 22:13:46 +0700

    Prepare project for Vercel deployment: Add environment variable and remove static export

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Remove output: "export" since Vercel supports dynamic apps
     images: {
         unoptimized: true, // Optional: Keep this if you don’t need image optimization
     },
